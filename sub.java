19b743dc0bd56bb6b744dad108e8bcdc169bf2c8
chaitra <chaitra.indi8@gmail.com>
2024-03-11 18:21:23 -0400

    publish to git

--- sub.java
+++ sub.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 
+<<<<<<< Updated upstream
 public class SubtractionWithScanner {
+=======
+public class SubtractionWithStaticVariables {
+
+    // Static variables to store the numbers
+    private static int num1;
+    private static int num2;
+
+    // Method to perform subtraction
+    public static int subtractNumbers() {
+        return num1 - num2;
+    }
+>>>>>>> Stashed changes
 
     public static void main(String[] args) {
         // Creating a Scanner object for user input
@@ -8,17 +21,30 @@ public class SubtractionWithScanner {
 
         // Taking input for the first number
         System.out.print("Enter the first number: ");
+<<<<<<< Updated upstream
         int num1 = scanner.nextInt();
 
         // Taking input for the second number
         System.out.print("Enter the second number: ");
         int num2 = scanner.nextInt();
+=======
+        num1 = scanner.nextInt();
+
+        // Taking input for the second number
+        System.out.print("Enter the second number: ");
+        num2 = scanner.nextInt();
+>>>>>>> Stashed changes
 
         // Closing the Scanner to avoid resource leakage
         scanner.close();
 
+<<<<<<< Updated upstream
         // Performing subtraction
         int difference = num1 - num2;
+=======
+        // Calling the method to subtract two numbers
+        int difference = subtractNumbers();
+>>>>>>> Stashed changes
 
         // Displaying the result
         System.out.println("The difference between " + num1 + " and " + num2 + " is: " + difference);
